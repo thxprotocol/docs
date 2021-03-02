41492ee6d1b742c76823364f34c5edc92947f0a8
Peter Polman <peter@peterpolman.nl>
2021-03-02 13:24:46 +0100

    Extends docs and alters layout

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,30 +1,23 @@
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Developer',
+  tagline: '',
+  url: 'https://docs.thx.network',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'thxprotocol', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Developers',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: 'THX Logo',
+        src: 'img/logo.png',
       },
       items: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {to: 'blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/thxprotocol/',
           label: 'GitHub',
           position: 'right',
         },
@@ -37,12 +30,16 @@ module.exports = {
           title: 'Docs',
           items: [
             {
-              label: 'Style Guide',
-              to: 'docs/',
+              label: 'Getting Started',
+              to: '/',
             },
             {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
+              label: 'API Specification',
+              to: '/swagger-docs',
+            },
+            {
+              label: 'Network Details',
+              to: '/networks',
             },
           ],
         },
@@ -50,16 +47,16 @@ module.exports = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: 'Medium',
+              href: 'https://medium.com/thxprotocol',
             },
             {
               label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              href: 'https://twitter.com/thxprotocol',
+            },
+            {
+              label: 'Slack',
+              href: 'https://thx.page.link/slack',
             },
           ],
         },
@@ -67,17 +64,13 @@ module.exports = {
           title: 'More',
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} THX.`,
     },
   },
   presets: [
@@ -85,16 +78,10 @@ module.exports = {
       '@docusaurus/preset-classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/thxprotocol/docs/edit/master/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,19 @@
 module.exports = {
-  someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
-  },
+  docs: [{
+      type: 'category',
+      label: 'Getting Started',
+      collapsed: false,
+      items: ['1.1.introduction', '1.2.example-usecase', '1.3.authorization', '1.4.example-request'],
+    },{
+      type: 'category',
+      label: 'API Specification',
+      collapsed: false,
+      items: ['2.1.swagger-docs', '2.2.asset_pools','2.3.members'],
+    },{
+      type: 'category',
+      label: 'Network Details',
+      collapsed: false,
+      items: ['3.1.networks','3.2.service-status', '3.3.api-health'],
+    }
+  ]
 };
